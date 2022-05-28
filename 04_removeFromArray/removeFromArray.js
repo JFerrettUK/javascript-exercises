a7c79bff7d765b914f5a84095c89e6a89738b6e5
JFerrettUK <jferrettuk@gmail.com>
2022-05-28 15:13:02 +0100

    removeFromArray exercise update

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,47 @@
-const removeFromArray = function() {
+
+
+const removeFromArray = function(testArr) {
+    for (let deleteNum = 0; deleteNum < testArr.length; deleteNum++) {
+
+        let args = Array.from(arguments)
+
+        console.log(args[4])
+        console.log(args[3])
+        console.log(args[2])
+        console.log(args[1])
+
+        if (testArr[deleteNum] == args[1]) {
+            testArr.splice(deleteNum,1);
+        } 
+        
+        if (testArr[deleteNum] == args[2]) {
+            testArr.splice(deleteNum,1);
+        } 
+        
+        if (testArr[deleteNum] == args[3]) {
+            testArr.splice(deleteNum,1);
+        } 
+
+        if (testArr[deleteNum] == args[4]) {
+        testArr.splice(deleteNum,1);
+      } 
+
+    }
+
+    return testArr
 
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+//Problem: removeFromArray([1, 2, 3, 4], 1, 2, 3, 4)
+
+//Plan:
+//1. Create a function.
+
+//2. Takes an array and some other arguments.
+
+//3. Removes the other arguments from that array
