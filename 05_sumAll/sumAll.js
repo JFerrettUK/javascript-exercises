a60ea09772650f406e2149fb47e5daf022b34fc0
JFerrettUK <jferrettuk@gmail.com>
2022-06-07 09:02:57 +0100

    sumAll up to Pass4

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,40 @@
-const sumAll = function() {
+const sumAll = function(firstDig, secondDig) {
+    if (firstDig <= 0) {
+        return "ERROR";
+    } else if (secondDig <= 0) {
+        return "ERROR";
+    }
 
-};
+    if (firstDig <= NaN) {
+        return "ERROR";
+    } else if (secondDig <= NaN) {
+        return "ERROR";
+    }
+
+    if (firstDig > secondDig) {
+        testDig = firstDig;
+        loopDig = secondDig;
+    } else if (secondDig > firstDig) {
+        testDig = secondDig;
+        loopDig = firstDig;
+    }
+
+    let addArray = [];
+    let sum = 0;
+    
+    while (loopDig <= testDig) {
+        addArray.push(loopDig)
+        loopDig++;
+    }
+
+    for (let i = 0; i < addArray.length; i++) {
+        sum += addArray[i];
+    }   
+    return sum;
+}
+
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
